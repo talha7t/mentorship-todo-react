abc55855293307df50db95e06dbcf3bbf51a39ff
talha7t <48598366+talha7t@users.noreply.github.com>
2020-10-01 18:08:53 +0500

    Updates

---
+++ precache-manifest.b0120f08a66c131c79c1c2de6c706014.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e66b27aaee57bdc0d3c7e31062143cda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09c39b816118acf0ee6d",
+    "url": "/static/css/main.86245e84.chunk.css"
+  },
+  {
+    "revision": "81c523fdcd943609582d",
+    "url": "/static/js/2.eddd12b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.eddd12b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09c39b816118acf0ee6d",
+    "url": "/static/js/main.57c9fb6f.chunk.js"
+  },
+  {
+    "revision": "559ff28579f192bdf8b9",
+    "url": "/static/js/runtime-main.98bec8e4.js"
+  }
+]);
